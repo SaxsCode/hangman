c66e382771e79896bf1a4657cee2e1290de0736b
SaxsCode <maxsax.sch@gmail.com>
2024-09-28 01:15:22 +0200

    feat: practice c fundementals

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, world!!");
+
+char greeting[] = "Hallo";
+
+char names[][10] = {"Max", "Kato", "Tijn"};
+
+int length = sizeof(names) / sizeof(names[0]);
+int i;
+
+for (int i = 0; i < length; i++) 
+{
+    if (strcmp(names[i], "Tijn") == 0)
+    {
+        continue;
+    }  
+
+    printf("%s %s\n", greeting, names[i]); 
+}
     return 0;
 }
