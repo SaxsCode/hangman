45ba103ea48587dd5d1ac025275ab7f108fbe7d5
SaxsCode <maxsax.sch@gmail.com>
2024-10-03 07:31:58 +0200

    feat(hangman): loop through words until all letters are guessed

--- src/hangman.c
+++ src/hangman.c
@@ -9,71 +9,51 @@ int getKeyByValue(char arr[], int size, char value);
 void removeElement(char arr[], int *size, char element);
 
 int main() {
+    srand(time(NULL));
 
-srand(time(NULL));
+    char words[][MAX_WORD_LENGTH] = {"boomhut", "apenstaart", "flapdrol", "hangman"}; 
+    int numWords = sizeof(words) / sizeof(words[0]);
 
-char words[][MAX_WORD_LENGTH] = {"boomhut", "apenstaart", "flapdrol", "hangman"}; 
-int numWords = sizeof(words) / sizeof(words[0]);
+    int randomKey = rand() % numWords;
+    const char *word = words[randomKey];
 
-int randomKey = rand() % numWords;
-const char *word = words[randomKey];
+    char haveToGuess[MAX_WORD_LENGTH];  
+    int wordLength = strlen(word);
 
+    printf("%s\n", word);
+    char userLetter;
 
-int haveToGuess[MAX_WORD_LENGTH];
-int wordLength = strlen(word);
-int guessLength = sizeof(haveToGuess) / sizeof(haveToGuess[0]);
+    for (int i = 0; i < wordLength; i) {  
+        printf("Guess a letter: \n");
+        scanf(" %c", &userLetter);
 
+        int key = getKeyByValue(word, wordLength, userLetter); 
 
-
-for (int i = 0; i < wordLength; i++) {
-    haveToGuess[i] = word[i];
-}
-
-printf("%s\n", word);
-printf("Characters to guess:\n");
-    for (int i = 0; i < wordLength; i++) {
-        printf("%d => %c\n", i, haveToGuess[i]);
-    }
-
-
-char userLetter;
-
-for (int i = 0; i < guessLength; i++) {
-
-    printf("Guess a letter: \n");
-    scanf(" %c", &userLetter);
-
-    int key = getKeyByValue(haveToGuess, &wordLength, userLetter);
-    printf("key = %c\n", key);
-
-    if (haveToGuess[key] != NULL)
-    {
-        printf("The word does contain:  %c\n", userLetter);   
-        removeElement(haveToGuess, &wordLength, userLetter);
-    } else 
-    {
-        printf("WRONG!");
-    }
-
-}
-
-return 0;
-
-}
-
-int getKeyByValue(int arr[], int size, int value) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value) {
-            return i;  // Return the index (key) if value is found
+        if (key != -1)
+        {
+            printf("The word does contain: %c\n", userLetter);   
+            removeElement(word, &wordLength, userLetter);
+        } else 
+        {
+            printf("WRONG!\n");
         }
     }
-    return -1;  // Return -1 if value is not found
+
+    return 0;
 }
 
-void removeElement(int arr[], int *size, int element) {
+int getKeyByValue(char arr[], int size, char value) {  
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void removeElement(char arr[], int *size, char element) {  
     int i, j, found = 0;
 
-    // Find the index of the element
     for (i = 0; i < *size; i++) {
         if (arr[i] == element) {
             found = 1;
@@ -81,16 +61,13 @@ void removeElement(int arr[], int *size, int element) {
         }
     }
 
-    // If element is found
     if (found) {
-        // Shift all elements after index i by one position to the left
         for (j = i; j < *size - 1; j++) {
             arr[j] = arr[j + 1];
         }
-        // Decrease the size of the array
         (*size)--;
-        printf("Element %d removed successfully.\n", element);
+        printf("Element %c removed successfully.\n", element);
     } else {
-        printf("Element %d not found in the array.\n", element);
+        printf("Element %c not found in the array.\n", element);
     }
 }
