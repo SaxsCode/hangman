c1d9beb51016c7ed447d4af67c22904c2b8b8fe8
SaxsCode <maxsax.sch@gmail.com>
2024-10-06 14:28:37 +0200

    feat(hangman): add replay functionality

--- src/hangman.c
+++ src/hangman.c
@@ -27,11 +27,13 @@ void displayGame(const GameState *game);
 void displayEndScreen(const GameState *game, bool won);
 void clearInputBuffer(void);
 int getUnguessedCount(const GameState *game);
+bool playAgainPrompt(void);
 
 int main() {
     WordsList wordsList;
     GameState game;
     char userLetter;
+    bool playAgain = true;
 
     // Load words from file
     if (!loadWords(&wordsList, "textfiles/words.txt")) {
@@ -40,27 +42,60 @@ int main() {
     }
     
     srand(time(NULL));
-    initializeGame(&game, &wordsList);
-    displayGame(&game);
 
-    while (getUnguessedCount(&game) > 0 && game.errors < MAX_ERRORS) {
-        printf("Guess a letter: ");
-        scanf(" %c", &userLetter);
-        
-        if (!makeGuess(&game, userLetter)) {
-            game.errors++;
-            printf("WRONG! Errors: %d/%d\n", game.errors, MAX_ERRORS);
-        }
-        
+    while (playAgain)
+    {
+        initializeGame(&game, &wordsList);
         displayGame(&game);
+
+        while (getUnguessedCount(&game) > 0 && game.errors < MAX_ERRORS) {
+            printf("Guess a letter: ");
+            scanf(" %c", &userLetter);
+
+            if (!makeGuess(&game, userLetter)) {
+                game.errors++;
+                printf("WRONG! Errors: %d/%d\n", game.errors, MAX_ERRORS);
+            }
+
+            displayGame(&game);
+        }
+
+        bool won = (getUnguessedCount(&game) == 0);
+        displayEndScreen(&game, won);
+
+        playAgain = playAgainPrompt();
+        if (playAgain) {
+            printf("\n=== Starting New Game ===\n\n");
+        }
     }
-
-    bool won = (getUnguessedCount(&game) == 0);
-    displayEndScreen(&game, won);
-
+    
+    printf("Thanks for playing! Goodbye!\n");
     return 0;
 }
 
+bool playAgainPrompt(void) {
+    char response;
+    bool validResponse = false;
+    bool playAgain = false;
+    
+    while (!validResponse) {
+        printf("Would you like to play again? (y/n): ");
+        scanf(" %c", &response);
+        clearInputBuffer();
+        
+        if (response == 'y' || response == 'Y') {
+            validResponse = true;
+            playAgain = true;
+        } else if (response == 'n' || response == 'N') {
+            validResponse = true;
+            playAgain = false;
+        } else {
+            printf("Invalid input. Please enter 'y' or 'n'.\n");
+        }
+    }
+    
+    return playAgain;
+}
 
 bool loadWords(WordsList *wordsList, const char *filename) {
     FILE *file = fopen(filename, "r");
@@ -127,10 +162,6 @@ void displayEndScreen(const GameState *game, bool won) {
     } else {
         printf("Oof! You failed! The word was: %s\n", game->word);
     }
-    
-    printf("Press Enter to end the program...");
-    clearInputBuffer();
-    getchar();
 }
 
 void clearInputBuffer(void) {
